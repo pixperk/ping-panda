0037edc87db93a0d74002f4031674f8540fc36a9
Yashaswi Kumar Mishra <mishrayashaswikumar@gmail.com>
2024-11-06 16:07:22 +0530

    add-event-category

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,32 +1,45 @@
-import { DashboardPage } from '@/components/dashboard-page'
-import { db } from '@/db'
-import { currentUser } from '@clerk/nextjs/server'
-import { redirect } from 'next/navigation'
+import { DashboardPage } from "@/components/dashboard-page"
+import { db } from "@/db"
+import { currentUser } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation"
 
-import { FC } from 'react'
-import DashboardPageContent from './dashboard-page-content'
+import { FC } from "react"
+import DashboardPageContent from "./dashboard-page-content"
+import { CreateEventCategoryModal } from "@/components/create-event-category-modal"
+import { Button } from "@/components/ui/button"
+import { PlusIcon } from "lucide-react"
 
-interface pageProps {
-  
-}
+interface pageProps {}
 
-const page: FC<pageProps> = async({}) => {
-    const auth = await currentUser()
-    if(!auth){
-        redirect("/sign-in")
-    }
+const page: FC<pageProps> = async ({}) => {
+  const auth = await currentUser()
+  if (!auth) {
+    redirect("/sign-in")
+  }
 
-    const user = await db.user.findUnique({
-        where:{externalId : auth.id}
-    })
+  const user = await db.user.findUnique({
+    where: { externalId: auth.id },
+  })
 
-    if(!user){
-        redirect('/sign-in')
-    }
+  if (!user) {
+    redirect("/sign-in")
+  }
 
-  return <DashboardPage title='Dashboard'>
-    <DashboardPageContent/>
-  </DashboardPage>
+  return (
+    <DashboardPage
+      cta={
+        <CreateEventCategoryModal>
+          <Button>
+            <PlusIcon className="size-4 mr-2" />
+            Add Category
+          </Button>
+        </CreateEventCategoryModal>
+      }
+      title="Dashboard"
+    >
+      <DashboardPageContent />
+    </DashboardPage>
+  )
 }
 
 export default page
--- src/server/routers/category-router.ts
+++ src/server/routers/category-router.ts
@@ -3,6 +3,8 @@ import { router } from "../__internals/router";
 import { privateProcedure } from "../procedures";
 import {startOfMonth} from "date-fns"
 import { z } from "zod";
+import { CATEGORY_NAME_VALIDATOR } from "@/lib/validators/category-validator";
+import { parseColor } from "@/lib/utils";
 
 export const categoryRouter = router({
     getEventCategories : privateProcedure.query(async({c,ctx})=>{
@@ -79,5 +81,32 @@ export const categoryRouter = router({
             })
             return c.json({success : true})
         }
-    )
+    ),
+
+    createEventCategory : privateProcedure.input(z.object({
+        name: CATEGORY_NAME_VALIDATOR,
+        color: z
+          .string()
+          .min(1, "Color is required")
+          .regex(/^#[0-9A-F]{6}$/i, "Invalid color format."),
+        emoji: z.string().emoji("Invalid emoji").optional(),
+    })).mutation(async({c,ctx,input})=>{
+        const {user} = ctx
+        const{color,name,emoji} = input
+
+        //ADD PAID PLAN LOGIC
+
+        const eventCategory = await db.eventCategory.create({
+            data:{
+                name : name.toLowerCase(),
+                color : parseColor(color),
+                emoji,
+                userId : user.id
+            }
+        })
+
+        return c.json({eventCategory})
+
+
+    })
 })
