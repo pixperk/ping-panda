fb14e26dabbea344ea33b02efa85147c445fa6e2
Yashaswi Kumar Mishra <mishrayashaswikumar@gmail.com>
2024-11-07 20:36:00 +0530

    events

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+export const FREE_QUOTA = {
+    maxEventsPerMonth : 100,
+    maxEventCategories : 3
+} as const
+export const PRO_QUOTA = {
+    maxEventsPerMonth : 1500,
+    maxEventCategories : 15
+} as const
